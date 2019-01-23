1bd41e5e5fb444e1d599a00e7e3c76a641a38ffb
chazzeromus <chaz.zeromus@gmail.com>
2019-01-23 00:54:15 -0500

    added storybook with template

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-/** @format */
+import { AppRegistry } from 'react-native';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// NOTE: Storybook cannot go after App or else we get a wierd react child
+// render error.
+import StorybookUI from './storybook';
+import App from './src/App';
+import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+
+AppRegistry.registerComponent(appName, () => __DEV__ ? StorybookUI : App);
