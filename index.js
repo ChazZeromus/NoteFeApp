8143ac0efae0cc5897efb114847647d3b0c3b020
chazzeromus <chaz.zeromus@gmail.com>
2019-01-24 00:57:45 -0500

    making storybook mode an explicit command and also starts web interface

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 import { AppRegistry } from 'react-native';
 
-// NOTE: Storybook cannot go after App or else we get a wierd react child
-// render error.
-import StorybookUI from './storybook';
 import App from './src/App';
 import { name as appName } from './app.json';
 
 
-AppRegistry.registerComponent(appName, () => __DEV__ ? StorybookUI : App);
+AppRegistry.registerComponent(appName, () => App);
