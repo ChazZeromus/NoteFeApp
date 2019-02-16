78b61b2cf5b4c032786d92ffdbaae8761aa5377a
chazzeromus <chaz.zeromus@gmail.com>
2019-02-16 16:23:43 -0500

    fixed inconsistent arching in stroke class

---
+++ src/Components/RadialScreens/styles.js
@@ -0,0 +1,22 @@
+// @flow
+import { StyleSheet } from 'react-native';
+
+const style : StyleSheet.Styles = {
+  dial: {
+    flex: 0,
+  },
+};
+
+export default StyleSheet.create(style);
+
+export const svgStyles = {
+  container: {
+    width: "150",
+    height: "150",
+  },
+};
+
+export const segmentStyle = {
+  x: 75,
+  y: 75,
+};
